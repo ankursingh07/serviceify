853a1423b8628e8bf075fe257f8e3a8673ff444f
Ankur Singh <ankursingh71101@gmail.com>
2022-07-14 22:22:23 +0530

    Second commit

---
+++ app/src/main/java/com/example/serviceify/HomeCustomer.java
@@ -0,0 +1,14 @@
+package com.example.serviceify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_customer);
+    }
+}
--- app/src/main/java/com/example/serviceify/MainActivity.java
+++ app/src/main/java/com/example/serviceify/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.serviceify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
+public class MainActivity extends AppCompatActivity
+{
+    private FirebaseAuth mAuth;
+    EditText email,password;
+    Button login;
+    String temp="";
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        getSupportActionBar().hide();
+        mAuth = FirebaseAuth.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        email=findViewById(R.id.login_email);
+        password=findViewById(R.id.login_password);
+        login=findViewById(R.id.login_button);
+        onLoginClick();
+    }
+    void onLoginClick()
+    {
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                temp=email.getText().toString();
+                //Toast.makeText(getApplicationContext(),temp,Toast.LENGTH_LONG).show();
+                Login();
+            }
+        });
+    }
+
+    void Login()
+    {
+
+        mAuth.signInWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    Toast.makeText(getApplicationContext(),"Successfully logged in",Toast.LENGTH_LONG).show();
+                }
+                else
+                    Toast.makeText(getApplicationContext(),"login failed",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
